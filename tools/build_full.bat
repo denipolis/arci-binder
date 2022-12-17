a12c063652db62c35a6121373c7ee7e770ffb122
Denis Polishchuk <52839937+denipolis@users.noreply.github.com>
2022-12-17 02:32:18 +0200

    Add files via upload

--- tools/build_full.bat
+++ tools/build_full.bat
@@ -2,6 +2,10 @@
 
 color 7
 
+mkdir build\cache
+mkdir build\cache2
+mkdir build\output
+
 "env\Scripts\python.exe" tools\compileUI.py
 "env\Scripts\pyinstaller.exe" --noconfirm main.spec
 "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" installer.iss 
