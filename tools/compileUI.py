e8c8d242bae3b4942dd7a56167fc926090c3645c
Denis Polishchuk <denipol07@gmail.com>
2023-09-16 16:04:42 +0300

    Move everything to src/

--- tools/compileUI.py
+++
@@ -1,9 +0,0 @@
-import os
-
-names = ['mainWindow', 'profileDeleteWindow', 'profileEditWindow', 'profileListWindow']
-
-for name in names:
-    os.system(f"{os.getcwd()}\\env\\Scripts\\pyuic6.exe -x ui/{name}.ui -o {name}.py")
-    print(f"\033[92m{name} - OK!")
-
-print("\033[0m")
