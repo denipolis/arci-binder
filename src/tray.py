90cd56b998e7d1d795c6e431eb5929b5427111ff
Denis Polishchuk <denipol07@gmail.com>
2023-09-24 05:34:48 +0300

    Tray redesign

--- src/tray.py
+++ src/tray.py
@@ -3,14 +3,16 @@ from PySide6.QtWidgets import QSystemTrayIcon, QMenu
 import os
 import utils
 
+import windows.resources_rc
+
 basedir = os.path.dirname(__file__)
 
 class TrayIcon(QSystemTrayIcon):
   def __init__(self, mainWindow):
-    super().__init__()
+    super(TrayIcon, self).__init__()
     self.setIcon(QIcon(os.path.join(basedir, "ui/images/logo32x32.png")))
-    trayShowAction = QAction("Открыть", icon=QIcon(QIcon(os.path.join(basedir, "ui/images/show.png"))), parent=self)
-    trayQuitAction = QAction("Выйти", icon=QIcon(QIcon(os.path.join(basedir, "ui/images/cross.png"))), parent=self)
+    trayShowAction = QAction(QIcon(u":/icons/images/eye.svg"), "Открыть", self)
+    trayQuitAction = QAction(QIcon(u":/icons/images/log-out.svg"), "Выйти", self)
 
     trayShowAction.triggered.connect(lambda: mainWindow.show())
     trayQuitAction.triggered.connect(lambda: utils.quit())
@@ -19,6 +21,7 @@ class TrayIcon(QSystemTrayIcon):
     self.activated.connect(lambda reason: mainWindow.show() if QSystemTrayIcon.ActivationReason.Trigger == reason else None)
 
     self.menu = QMenu()
+    self.menu.setStyleSheet('color: #f5f5f5; font-family: Rubik; background-color: #13171c;')
     self.menu.addAction("ArciBinder").setEnabled(False)
     self.menu.addSeparator()
     self.menu.addAction(trayShowAction)
