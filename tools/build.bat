517eb0b8e199b7796ecfe71356004f613c61a09b
Denis Polishchuk <denipol07@gmail.com>
2023-10-05 03:19:25 +0300

    fixed crash on start

--- tools/build.bat
+++ tools/build.bat
@@ -4,12 +4,12 @@ mkdir build\workcache
 mkdir build\runcache
 mkdir build\dist
 
-rem call "tools\translateUI.bat"
-rem call "env\Scripts\pyinstaller.exe" --log-level DEBUG --noconfirm main.spec
+call "tools\translateUI.bat"
+call "env\Scripts\pyinstaller.exe" --log-level DEBUG --noconfirm main.spec
 call "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" installer.iss 
 
-rem rd /s /q dist
-rem rd /s /q build\main
-rem rd /s /q build\workcache
-rem rd /s /q build\runcache
-rem rd /s /q build\dist
+rd /s /q dist
+rd /s /q build\main
+rd /s /q build\workcache
+rd /s /q build\runcache
+rd /s /q build\dist
